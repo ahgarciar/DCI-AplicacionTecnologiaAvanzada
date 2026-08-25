2c6f3134bf12e47cf7e3db58c31dff6414b908d7
ahgarciar <ahgarcia@docentes.uat.edu.mx>
2026-08-25 08:13:03 -0600

    up

--- P6_KnnImplementation.py
+++ P6_KnnImplementation.py
@@ -1,5 +1,5 @@
 
-
+#Valores de entrada
 X = [[0], [1], [2], [3]]
 y = [0, 0, 1, 1]
 
@@ -8,8 +8,15 @@ print(X)
 print("y:")
 print(y)
 
+# KNN = K vecinos más cercanos
 from sklearn.neighbors import KNeighborsClassifier
-neigh = KNeighborsClassifier(n_neighbors=3)
+neigh = KNeighborsClassifier(n_neighbors=3) # K = 3
+
 neigh.fit(X, y)
-print(neigh.predict([[1.1]]))
+
+newCase = [[1.1]]
+predicted_class = neigh.predict(newCase)
+print(predicted_class)
+
 print(neigh.predict_proba([[0.9]]))
+
