008e8393c48999ccc458c8d10b18e605181a1fc8
ahgarciar <ahgarcia@docentes.uat.edu.mx>
2026-09-07 08:09:26 -0600

    up

---
+++ P11_YData/Profiling.py
@@ -0,0 +1,16 @@
+import pandas as pd
+from ydata_profiling import ProfileReport
+
+df = pd.read_csv("../iris/iris.csv")
+
+reporte = ProfileReport(
+    df,
+    title="Análisis exploratorio de datos",
+    explorative=True
+)
+
+# Para Jupyter
+#reporte.to_notebook_iframe()
+
+#Para HTML
+reporte.to_file("reporte_explorative.html")
